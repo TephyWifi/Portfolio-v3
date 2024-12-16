c0b26e0f376b57e7c713005e13ddd088f264a2c1
Stephanie <tephyreiso@proton.me>
2024-12-16 14:29:20 +0100

    Worked on mobile pge for index ++

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // Navbar
+
+
 function toggleNav() {
     var element = document.getElementById("myOverlaynav")
     var shown = element.getAttribute("data-isshown");
     if (shown == "true") {
         element.setAttribute("data-isshown", "false");
         element.style.width = "0";
+        document.querySelector('#myMenu').textContent = 'MENU';
     }
     else {
         element.setAttribute("data-isshown", "true");
         element.style.width = "100%";
+        document.querySelector('#myMenu').textContent = 'CLOSE';
     }
 }
 
