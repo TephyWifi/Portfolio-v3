a27d3c4fec6a889d8c7de7fe328ed26d8a37034f
Stephanie <tephyreiso@proton.me>
2025-01-15 12:12:15 +0100

    Fixed slideshow

--- js/aboutscript.js
+++ js/aboutscript.js
@@ -18,14 +18,27 @@ element.addEventListener("click", function toggleNav() {
 
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("darkIcon");
+    const body = document.body;
+
+    if (localStorage.getItem("darkmode") === "enabled") {
+        body.classList.add("switchmode");
+    }
+
     button.addEventListener("click", () => {
-        document.getElementById("darkIcon");
-        let element = document.body;
-        element.classList.toggle("switchmode")
+        body.classList.toggle("switchmode");
+
+        if (body.classList.contains("switchmode")) {
+            localStorage.setItem("darkMode", "enabled");
+        }
+        else {
+            localStorage.setItem("darkmode", "disabled");
+        }
+        // document.getElementById("darkIcon");
+        // let element = document.body;
+        // element.classList.toggle("switchmode")
     });
 });
 
-// Slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -38,19 +51,35 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    const slides = document.querySelectorAll(".mySlides");
+    const dots = document.querySelectorAll(".dot");
     
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length} ;
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) { 
-        dots[i].classList.remove("slideractive");
-    }
-    
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;} 
+
+    slides.forEach(slide => slide.style.display = "none");
+    dots.forEach(dot => dot.classList.remove("active"));
+
     slides[slideIndex-1].style.display="block";
-    dots[slideIndex-1].classList.add("slideractive");
+    dots[slideIndex-1].classList.add("active");
 }
+
+document.querySelector(".slideshow-container").addEventListener("click", function (event) {
+    const target = event.target;
+
+    if (target.classList.contains("prev")) {
+        plusSlides(-1);
+    }
+    else if (target.classList.contains("next")) {
+        plusSlides(1);
+    }
+});
+
+document.querySelector(".dot-container").addEventListener("click", function (event) {
+    const target = event.target;
+
+    if (target.classList.contains("dot")) {
+        const slideIndex = parseInt(target.getAttribute("data-slide"));
+        currentSlide(slideIndex);
+    }
+});
